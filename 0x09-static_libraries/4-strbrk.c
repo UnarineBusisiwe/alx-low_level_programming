83e0b3bd5a929e577e1afba37a6d272f57665f60
UnarineBusisiwe <ubmbuvha@gmail.com>
2023-09-29 07:13:31 -0700

    create_static_lib.sh

--- 0x09-static_libraries/4-strbrk.c
+++ 0x09-static_libraries/4-strbrk.c
@@ -1,10 +1,10 @@
 #include "main.h"
-#include <stdio.>
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
 char *_strbrk(char *s, char *accept)
 {
-	return strbrk(s, accept);
+	return _strbrk(s, accept);
 }
