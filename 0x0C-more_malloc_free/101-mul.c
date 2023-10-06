cd1377e90ab13c4cb922d94aa1519e16fad13518
UnarineBusisiwe <ubmbuvha@gmail.com>
2023-10-06 11:54:58 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,8 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+
+#define ERR_MSG "Error"
 
 /**
  * is_digit - checks if a string contains a non-digit char
@@ -37,8 +39,9 @@ int _strlen(char *s)
 	}
 	return (i);
 }
+
 /**
- * errors - handles errors for
+ * errors - handles errors for main
  */
 void errors(void)
 {
@@ -52,15 +55,15 @@ void errors(void)
  * @agrv: array of arguments
  *
  *
- * Return: always 0 (Successs)
+ * Return: always 0 (Success)
  */
-int main(int argc, char *argv[j])
+int main(void)
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2)
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
