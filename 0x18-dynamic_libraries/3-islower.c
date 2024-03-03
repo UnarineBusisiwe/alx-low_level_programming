fadf8272136a2a2e6e03c73a404467389b3138c0
UnarineBusisiwe <ubmbuvha@gmail.com>
2024-03-03 10:50:38 -0800

    update

--- 0x18-dynamic_libraries/3-islower.c
+++ 0x18-dynamic_libraries/3-islower.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-char *_strpbrk(char *s, char *accept)
+int _ialower(int c)
 {
-	return strpbrk(s, accept);
+	return (c >= 'a' && c <= 'z');
 }
